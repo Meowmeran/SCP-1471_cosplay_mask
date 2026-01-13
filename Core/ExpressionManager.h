8c8993e8e68d7f368569ca0653c070588c7c9749
Kedi <65986751+Meowmeran@users.noreply.github.com>
2026-01-13 04:04:00 +0300

    Add expression selection methods to ExpressionManager

--- Core/ExpressionManager.h
+++ Core/ExpressionManager.h
@@ -11,13 +11,21 @@ namespace Core
     private:
         uint32_t lastUpdateTime;
         Expressions::Type currentExpression;
+        Expressions::Type selectedExpression;
         MaskFrame *frame;
 
     public:
         ExpressionManager(MaskFrame* frame) : currentExpression(Expressions::Type::Neutral), frame(frame) {}
 
+        void setExpression(); // Sets to selectedExpression
         void setExpression(Expressions::Type type);
-        Expressions::Type getCurrentExpression() const;
+        Expressions::Type nextExpression();
+        Expressions::Type previousExpression();
+        void selectExpression(Expressions::Type type);
+        Expressions::Type selectNextExpression();
+        Expressions::Type selectPreviousExpression();
+        Expressions::Type getSelectedExpression() const { return selectedExpression; }
+        Expressions::Type getCurrentExpression() const { return currentExpression; }
         void updateFrame();
     };
 }
