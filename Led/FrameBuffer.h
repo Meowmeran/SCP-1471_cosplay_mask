d7d3350cdbdb3b9457fdebbf96c0d809b37ea976
Kedi <65986751+Meowmeran@users.noreply.github.com>
2026-01-15 01:17:00 +0300

    Refactor getErrorFrame for clarity and efficiency
    Refactor getErrorFrame function to use static lastUpdateTime and constant timeThreshold. Simplify frame clearing logic.

--- Led/FrameBuffer.h
+++ Led/FrameBuffer.h
@@ -29,30 +29,25 @@ struct MaskFrame
 
 static MaskFrame getErrorFrame()
 {
-  uint32_t timeThreshold = 1000; // 1 second blink interval
-  uint32_t lastUpdateTime = millis();
+  static uint32_t lastUpdateTime = 0;
+  const uint32_t timeThreshold = 1000; // 1 second blink interval
+  
   MaskFrame frame;
   frame.clear();
+  
   if (((millis() - lastUpdateTime) % (2 * timeThreshold)) < timeThreshold)
   {
+    // Fill diagonal with red
     for (uint8_t i = 0; i < 4; i++)
     {
       frame.left[i][i] = {255, 0, 0};
       frame.right[i][i] = {255, 0, 0};
     }
-    return frame;
   }
   else
   {
-    for (uint8_t i = 0; i < 4; i++)
-    {
-      for (uint8_t j = 0; j < 4; j++)
-      {
-        frame.left[i][j] = {0, 0, 0};
-        frame.right[i][j] = {0, 0, 0};
-      }
-    }
     lastUpdateTime = millis();
-    return frame;
   }
+  
+  return frame;
 }
